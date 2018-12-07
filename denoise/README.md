1d2ce22bb057d7ba486f983f610e70e742598721
nanHome <nan@nanhome.com>
2018-12-07 22:56:31 +0800

    speech repos; features; denoise;

---
+++ denoise/README.md
@@ -0,0 +1,15 @@
+# directly Denoise AutoEncoder.
+  code:
+    1 RNN Denoise
+      https://github.com/Nisoka/rnnoise
+      need a pipeline usage!
+    
+  
+# use speech separate as a Denoise method
+  
+  check the mozilla bookmark of Speech/speech-separate
+  
+  code:
+    1 VPNN + interface Mask loss
+    https://andabi.github.io/music-source-separation/
+    2 
---
+++ features/pylibrosa/nlibrosa.py
@@ -0,0 +1,33 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+
+
+def show_wave_logMelSpec_of(wavPath):
+    # Load a wav file
+    y, sr = librosa.load(wavPath, sr=None)
+    # extract mel spectrogram feature
+    melspec = librosa.feature.melspectrogram(y, sr, n_fft=1024, hop_length=512, n_mels=128)
+    # convert to log scale
+    logmelspec = librosa.power_to_db(melspec)
+    plt.figure()
+    # plot a wavform
+    plt.subplot(2, 1, 1)
+    librosa.display.waveplot(y, sr)
+    plt.title(wavPath)
+    # plot mel spectrogram
+    plt.subplot(2, 1, 2)
+    librosa.display.specshow(logmelspec, sr=sr, x_axis='time', y_axis='mel')
+    plt.title('Mel spectrogram')
+    plt.tight_layout() #保证图不重叠
+# ---------------------
+# 作者：z小白
+# 来源：CSDN
+# 原文：https://blog.csdn.net/zzc15806/article/details/79603994
+# 版权声明：本文为博主原创文章，转载请附上博文链接！
+
+
+if __name__ == '__main__':
+    show_wave_logMelSpec_of('./data/input.wav')
+    show_wave_logMelSpec_of('./data/output.wav')
+    plt.show()
